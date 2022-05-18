99bfbcf85e50dde85a9c0a6e8a0de864931ddfd6
Jetsurf <crmeade@mtu.edu>
2022-05-18 00:33:54 +0000

    Add bot used endpoints for s2

--- pynso/s2_api.py
+++ pynso/s2_api.py
@@ -15,7 +15,7 @@ class Splatoon2():
 			'Accept-Encoding': 'gzip, deflate',
 			'Accept-Language': 'en-us'
 		}
-		self.ah_s2_shop = {
+		self.s2_shop = {
 			"origin": "https://app.splatoon2.nintendo.net",
 			"x-unique-id": '16131049444609162796',
 			"x-requested-with": "XMLHttpRequest",
@@ -27,7 +27,7 @@ class Splatoon2():
 			"Accept-Language": "en-US"
 		}
 
-		self.ah_s2_coop = {
+		self.s2_coop = {
 			'Host': 'app.splatoon2.nintendo.net',
 			'x-unique-id': '8386546935489260343',
 			'x-requested-with': 'XMLHttpRequest',
@@ -39,17 +39,33 @@ class Splatoon2():
 			'Accept-Language': 'en-us'
 		}
 
-	def get_player_stats(self, iksm) -> dict:
-		print(type(iksm))
-		results_list = requests.get("https://app.splatoon2.nintendo.net/api/records", headers=self.s2_player, cookies=dict(iksm_session=iksm['iksm']))
-		thejson = json.loads(results_list.text)
+	def get_player_records(self, iksm) -> dict:
+		response = requests.get("https://app.splatoon2.nintendo.net/api/records", headers=self.s2_player, cookies=dict(iksm_session=iksm['iksm']))
+		thejson = json.loads(response.text)
 		return thejson
 
 	def get_salmon_run(self, iksm) -> dict:
-		pass
+		response = requests.get("https://app.splatoon2.nintendo.net/api/coop_results", headers=self.s2_coop, cookies=dict(iksm_session=iksm['iksm']))
+		thejson = json.loads(response.text)
+		return thejson
 
 	def get_battles(self, iksm) -> dict:
-		pass
+		response = requests.get("https://app.splatoon2.nintendo.net/api/results", headers=self.s2_player, cookies=dict(iksm_session=iksm['iksm']))
+		thejson = json.loads(response.text)
+		return thejson
 
-	def get_full_battles(self, iksm) -> dict:
-		pass
+	def get_full_battles(self, battleid, iksm) -> dict:
+		response = requests.get(f"https://app.splatoon2.nintendo.net/api/results/{battleid}", headers=self.s2_player, cookies=dict(iksm_session=iksm['iksm']))
+		thejson = json.loads(response.text)
+		return thejson
+
+	def get_store_merch(self, iksm) -> dict:
+		response = requests.get("https://app.splatoon2.nintendo.net/api/onlineshop/merchandises", headers=self.s2_player, cookies=dict(iksm_session=iksm['iksm']))
+		thejson = json.loads(response.text)
+		return thejson
+
+	def order_from_store(self, gearid, override=False, iksm=None) -> dict:
+		payload = { "override" : "1" if override else "0" }
+		response = requests.post(f"https://app.splatoon2.nintendo.net/api/onlineshop/order/{gearid}", headers=app_head, cookies=dict(iksm_session=iksm['iksm']), data=payload)
+
+		return json.loads(response.text)
