80b1bbcdf069b8333c104061947daf04f3e2051c
Andrew Wendt <andy@putergeek.com>
2023-02-08 02:20:14 -0800

    Bump version number.

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import find_packages, setup
+
+with open('nso_api/version.py') as f:
+	exec(f.read())
+
 setup(
     name='nso-api',
     packages=find_packages(include=['nso_api']),
-    version='0.9.2',
+    version = __version__,
     description='Nintendo Switch Online API Python Library',
     author='jetsurf#8514, Andy#3003',
     license='GPL',
